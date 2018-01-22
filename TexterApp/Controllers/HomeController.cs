f2c5ff1ce7ab628b2db3cb4a55ef3998d8267ba0
Gyles Batara <gyles.batara@live.com>
2018-01-22 14:57:52 -0800

    Should be able to send and view messages now

---
+++ TexterApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TexterApp.Models;
+
+namespace TexterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Viewing Messages
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetMessages()
+        {
+            var allMessages = Message.GetMessages();
+            return View(allMessages);
+        }
+
+        //Sending Messages
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Message newMessage)
+        {
+            newMessage.Send();
+            return RedirectToAction("Index");
+        }
+
+        //sfsd
+    }
+}
---
+++ TexterApp/Models/Message.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TexterApp.Models
+{
+    public class Message
+    {
+		public string To { get; set; }
+		public string From { get; set; }
+		public string Body { get; set; }
+		public string Status { get; set; }
+
+        public static List<Message> GetMessages()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest("Accounts/ACa2cf9565f661f8e1ba285ad71bf95617/Messages.json", Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator("ACa2cf9565f661f8e1ba285ad71bf95617", "552015e2fc09d2bfcb86ec93e12b463b");
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var messageLIst = JsonConvert.DeserializeObject<List<Message>>(jsonResponse["messages"].ToString());
+            return messageLIst;
+        }
+
+        public void Send()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest("Accounts/ACa2cf9565f661f8e1ba285ad71bf95617/Messages", Method.POST);
+            request.AddParameter("To", To);
+            request.AddParameter("From", From);
+            request.AddParameter("Body", Body);
+            client.Authenticator = new HttpBasicAuthenticator("ACa2cf9565f661f8e1ba285ad71bf95617", "552015e2fc09d2bfcb86ec93e12b463b");
+            client.ExecuteAsync(request, response => {
+                Console.WriteLine(response.Content);
+            });
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
